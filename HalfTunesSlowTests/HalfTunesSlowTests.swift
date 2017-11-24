3767e1274734f8c4f4ad542d442b412756159c98
Seto Elkahfi <setoelkahfi@gmail.com>
2017-11-24 13:48:35 +0700

    Using XCTestExpectation to Test Asynchronous Operations'

---
+++ HalfTunesSlowTests/HalfTunesSlowTests.swift
@@ -0,0 +1,64 @@
+//
+//  HalfTunesSlowTests.swift
+//  HalfTunesSlowTests
+//
+//  Created by SDMobile on 24/11/17.
+//  Copyright © 2017 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+@testable import HalfTunes
+
+class HalfTunesSlowTests: XCTestCase {
+    
+    var sessionUnderTest: URLSession!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sessionUnderTest = URLSession(configuration: .default)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sessionUnderTest = nil
+        super.tearDown()
+    }
+    
+    // Asynchronous test: success fast, failure slow
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        // given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { (data, response, error) in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        
+        // 3
+         waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
